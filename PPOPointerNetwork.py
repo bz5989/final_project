fac78462122b01e5f16c352e2de1094fb2e2f915
Bryan Zhang <bz5989@princeton.edu>
2026-04-24 03:40:40 -0400

    simplified / modified network

--- PPOPointerNetwork.py
+++ PPOPointerNetwork.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from PPOSchedulerAgent import PPOSchedulerAgent
 from Environment import Environment
+from utils import plot_diagnostics
 from Core import TaskCategory, TaskGenerator, linear_penalty
 
 class PPO_Pointer_Network(nn.Module):
@@ -10,11 +11,10 @@ class PPO_Pointer_Network(nn.Module):
         super().__init__()
         self.H = H
         
-        self.embedding = nn.Embedding(H, emb)
-        self.encoder = nn.LSTM(emb, hid, batch_first=True)
+        self.encoder = nn.LSTM(input_size=2, hidden_size=hid, batch_first=True)
 
         self.job_encoder = nn.Sequential(
-            nn.Linear(3, hid), 
+            nn.Linear(2, hid), 
             nn.ReLU(), 
             nn.Linear(hid, hid)
         )
@@ -33,36 +33,40 @@ class PPO_Pointer_Network(nn.Module):
         )
 
     def encode(self, s):
-        return self.encoder(self.embedding(s.unsqueeze(0)))[0].squeeze(0)
+        if s.dim() == 1:
+            s = s.unsqueeze(0)
+        
+        enc, _ = self.encoder(s)
+        return enc
 
     def forward(self, schedule, job, mask=None):
         enc = self.encode(schedule)
+        job_vec = self.job_encoder(job)
 
-        if enc.dim() == 2:
-            enc = enc.unsqueeze(0)
-
-        B = enc.size(0)
-
-        if job is None:
-            job_vec = torch.zeros(B, enc.size(-1))
-        else:
-            job_vec = self.job_encoder(job)
-
-        job_query = self.W_query(job_vec).unsqueeze(1)
         schedule_meaning = self.W_ref(enc)
+        job_query = self.W_query(job_vec).unsqueeze(-2)
+        # print(enc.shape, job_vec.shape)
+        # print(schedule_meaning.shape, job_query.shape)
 
         u = torch.tanh(schedule_meaning + job_query)
         logits = torch.matmul(u, self.v)
 
         if mask is not None:
+            mask = mask.float()
             logits = logits + (mask - 1) * 1e9
 
-        value = self.value_head(enc.mean(dim=1)).squeeze(-1)
-        return logits, value
+        return logits
     
+    def get_value(self, schedule):
+        enc = self.encode(schedule)
+        pool = enc.mean(dim=1)
+        value = self.value_head(pool).squeeze(-1)
+        return value
+
     def get_pred_duration(self, job):
-        if job is None:
-            return 0
+        if job.dim() == 1:
+            job = job.unsqueeze(0)
+
         job_vec = self.job_encoder(job)
         pred_duration = self.duration_head(job_vec).squeeze(-1)
         return pred_duration
@@ -74,75 +78,79 @@ class PPO_Pointer_Network(nn.Module):
 
     @torch.no_grad()
     def get_action(self, schedule, job, mask=None):
-        logits, value = self.forward(schedule, job, mask)
+        logits = self.forward(schedule, job, mask)
         dist = torch.distributions.Categorical(logits=logits)
         action = dist.sample()
-        return action, dist.log_prob(action), value
+        return action, dist.log_prob(action)
 
     def get_log_prob(self, schedule, job, action, mask=None):
-        logits, value = self.forward(schedule, job, mask)
+        logits = self.forward(schedule, job, mask)
         dist = torch.distributions.Categorical(logits=logits)
-        return dist.log_prob(action), value
+        return dist.log_prob(action)
 
 def collect_batch(env, scheduler, model, num_steps=100, gamma=0.99, lam=0.95):
     schedules, actions, log_probs = [], [], []
     rewards, values, jobs, masks = [], [], [], []
     pred_durations, true_lengths = [], []
-    
-    allocation = {}
+    valid = []
 
-    schedule = scheduler.reset()
-    env.t = 0
+    scheduler.reset()
 
     for _ in range(num_steps):
-        schedule_tensor = torch.tensor(scheduler.embed_schedule())
-        job = env.sample_job()
-        job_tensor = torch.tensor([job.task_category, job.deadline_time, job.base_reward]) if job else torch.zeros(3)
-        pred_dur = model.get_pred_duration(job_tensor)
-        pred_length = int(torch.ceil(pred_dur).item())
-        mask_tensor = torch.tensor(scheduler.valid_mask(pred_length))
-        action, log_prob, value = model.get_action(schedule_tensor, job_tensor, mask_tensor)
-        # action is equivalent to location where placed
+        schedule_tensor = torch.tensor(scheduler.embed_schedule(), dtype=torch.float32)
+        id, job = env.sample_job()
+
         reward = 0
+        value = model.get_value(schedule_tensor.unsqueeze(0)).squeeze(0).detach()
+
+        job_tensor = torch.tensor([-1, 0])
+        action = torch.tensor(0)
+        log_prob = torch.tensor(0.0)
+        mask_tensor = torch.zeros_like(torch.tensor(scheduler.valid_mask(1)))
 
         if job:
-            jid = job.instance_id
+            # print(job.task_id)
+            job_tensor = torch.tensor([job.task_id, job.deadline_time - scheduler.t])
+
+            pred_dur = model.get_pred_duration(job_tensor)
+            pred_length = int(torch.ceil(pred_dur).item())
+
+            mask_tensor = torch.tensor(scheduler.valid_mask(pred_length))
+            action, log_prob = model.get_action(schedule_tensor, job_tensor, mask_tensor)
+
+            # action is equivalent to location where placed
             if scheduler.can_place(action, pred_length):
-                scheduler.place(jid, pred_length, action)
-
-                allocation[jid] = allocation.get(jid, 0) + pred_length
-
-                pred_durations.append(pred_dur)
-                true_lengths.append(torch.tensor(job.duration, dtype=torch.float32))
+                scheduler.place(job, pred_length, action)
             else:
                 reward -= job.base_reward
-
-        finished = scheduler.shift()
+            
+            pred_durations.append(pred_dur.detach())
+            true_lengths.append(torch.tensor(job.duration_time, dtype=torch.float32))
+            valid.append(1)
+        else:
+            pred_durations.append(torch.tensor([0.0]))
+            true_lengths.append(torch.tensor(0.0))
+            valid.append(0)
+        
+        worked_job = scheduler.shift()
         env.step_time()
 
         # penalize underestimates of jobs
-        if finished != 0:
-            jid = finished
-            if jid in env.jobs:
-                true_length = env.jobs[jid]["length"]
-                alloc = allocation.get(jid, 0)
-                if alloc < true_length:
-                    reward -= (true_length - alloc)
+        if worked_job != -1:
+            reward += scheduler.check_reward(worked_job)
 
-        reward += env.lateness_reward()
-        schedule = scheduler.get_schedule_window()
+        # reward += env.lateness_reward()
 
         schedules.append(schedule_tensor)
-        actions.append(action)
-        log_probs.append(log_prob)
+        actions.append(action.squeeze())
+        log_probs.append(log_prob.squeeze())
         rewards.append(reward)
         values.append(value)
         jobs.append(job_tensor)
         masks.append(mask_tensor)
     
-    job = env.sample_job()
-    mask_tensor = torch.tensor(scheduler.valid_mask(job))
-    _, _, value = model.get_action(torch.tensor(schedule), job, mask_tensor)
+    schedule_tensor = torch.tensor(scheduler.embed_schedule(), dtype=torch.float32)
+    value = model.get_value(schedule_tensor.unsqueeze(0)).squeeze(0).detach()
     values.append(value)
 
     advantages = []
@@ -153,64 +161,112 @@ def collect_batch(env, scheduler, model, num_steps=100, gamma=0.99, lam=0.95):
         advantages.insert(0, gae)
     
     returns = [a+v for a,v in zip(advantages, values[:-1])]
+
+    advantages = torch.tensor(advantages)
+    returns = torch.tensor(returns)
+
     advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
 
     batch = {
-        'schedules': schedules,
-        'actions': actions,
-        'log_probs': log_probs,
-        'jobs': jobs,
-        'masks': masks,
+        'schedules': torch.stack(schedules),
+        'actions': torch.stack(actions),
+        'log_probs': torch.stack(log_probs),
+        'jobs': torch.stack(jobs),
+        'masks': torch.stack(masks),
         'returns': returns,
         'advantages': advantages,
-        'pred_durations': pred_durations,
-        'true_lengths': true_lengths
+        'pred_durations': torch.stack(pred_durations),
+        'true_lengths': torch.stack(true_lengths),
+        'valid': torch.tensor(valid)
     }
 
     return batch
 
 def ppo_update(model, optimizer, batch, clip_epsilon=0.2, epochs=4, batch_size=32, value_coef=0.5, dur_coef=0.1):
-    schedules = torch.stack(batch["schedules"])
-    actions = torch.tensor(batch["actions"])
-    old_logp = torch.tensor(batch["log_probs"])
-    jobs = torch.stack(batch["jobs"])
-    masks = torch.tensor(batch["masks"])
-    returns = torch.tensor(batch["returns"])
-    advantages = torch.tensor(batch["advantages"])
+    schedules = batch["schedules"]
+    actions = batch["actions"]
+    old_logp = batch["log_probs"]
+    jobs = batch["jobs"]
+    masks = batch["masks"]
+    returns = batch["returns"]
+    advantages = batch["advantages"]
 
-    pred_durations = torch.stack(batch["pred_durations"])
-    true_lengths = torch.stack(batch["true_lengths"])
+    pred_durations = batch["pred_durations"]
+    true_lengths = batch["true_lengths"]
 
-    idx = torch.randperm(schedules)
+    valid = batch["valid"]
 
+    policy_losses = 0.0
+    value_losses = 0.0
+    dur_losses = 0.0
+
+    num_batches = len(schedules) / batch_size
     for _ in range(epochs):
+        idx = torch.randperm(len(schedules))
         for i in range(0, len(schedules), batch_size):
             batch_indices = idx[i:i + batch_size]
-            new_log_probs, values = model.get_log_prob(schedules[batch_indices], jobs[batch_indices], 
+            new_log_probs = model.get_log_prob(schedules[batch_indices], jobs[batch_indices], 
                                            actions[batch_indices], masks[batch_indices])
             
+            new_durations = model.get_pred_duration(jobs[batch_indices])
+            values = model.get_value(schedules[batch_indices])
+
             ratio = torch.exp(new_log_probs - old_logp[batch_indices])
             s1 = ratio * advantages[batch_indices]
             s2 = torch.clamp(ratio, 1-clip_epsilon, 1+clip_epsilon) * advantages[batch_indices]
 
             policy_loss = -torch.mean(torch.min(s1, s2))
-            value_loss = nn.mse_loss(values, returns[batch_indices])
-            dur_loss = nn.mse_loss(pred_durations, true_lengths)
+            value_loss = F.mse_loss(values, returns[batch_indices])
+            dur_loss = F.mse_loss(new_durations, true_lengths[batch_indices])
 
             loss = policy_loss + value_coef * value_loss + dur_coef * dur_loss
+
+            policy_losses += policy_loss.item()
+            value_losses += value_loss.item()
+            dur_losses += dur_loss.item()
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+    return {'policy_loss': policy_losses / epochs / num_batches,
+            'value_loss': value_losses / epochs / num_batches,
+            'dur_loss': dur_losses / epochs / num_batches}
 
 def train_ppo(model, optimizer, scheduler, env):
+    # training_rewards = []
+    metrics = {'policy_loss': [], 'value_loss': [], 'dur_loss': []}
     for ep in range(200):
-        batch = collect_batch(env, scheduler, model)
-        ppo_update(model, optimizer, batch)
+        batch = collect_batch(env, scheduler, model, num_steps=200)
+        dict = ppo_update(model, optimizer, batch)
+        
+        for key in metrics:
+            metrics[key].append(dict[key])
 
         if ep % 20 == 0:
             print("Episode", ep)
 
+    return metrics
+
+import random
+class MicroEnv:
+    def __init__(self, env):
+        self.time = 0
+        self.jobs = 0
+        self.env = env
+    
+    def sample_job(self):
+        items = self.env.get_timestep(self.time).task_instances
+        if len(items) == 0:
+            return -1, None
+        elif len(items) == 1:
+            self.jobs += 1
+            return self.jobs - 1, items[0]
+        rand = random.randint(0, len(items) - 1)
+        self.jobs += 1
+        return self.jobs - 1, items[rand]
+    
+    def step_time(self):
+        self.time += 1
 
 if __name__ == "__main__":
     H = 8
@@ -222,9 +278,12 @@ if __name__ == "__main__":
     # environment taken from runner.py
     quick_tasks = TaskCategory(
         name="Quick",
+        task_id=1,
         category_seed=1,
         mean_time=1,
         std_time=0.3,
+        mean_buffer_time=1.0,
+        std_buffer_time=0.4,
         mean_reward=3.0,
         std_reward=0.5,
         penalty_fn=linear_penalty
@@ -232,9 +291,12 @@ if __name__ == "__main__":
 
     big_tasks = TaskCategory(
         name="Big",
+        task_id=2,
         category_seed=2,
         mean_time=5,
         std_time=1.0,
+        mean_buffer_time=1.0,
+        std_buffer_time=0.4,
         mean_reward=10.0,
         std_reward=1.5,
         penalty_fn=linear_penalty
@@ -244,5 +306,8 @@ if __name__ == "__main__":
         TaskGenerator(quick_tasks, generator_seed=10, probability=0.3),
         TaskGenerator(big_tasks, generator_seed=11, probability=0.1)
     ]
-    env = Environment(generators=generators, timesteps=1000)
-    train_ppo(model, optimizer, scheduler, env)
+    env = Environment(generators=generators, timesteps=40000)
+    env_wrapper = MicroEnv(env)
+    metrics = train_ppo(model, optimizer, scheduler, env_wrapper)
+
+    plot_diagnostics(metrics, "Test", "diagnostics.png")
--- PPOSchedulerAgent.py
+++ PPOSchedulerAgent.py
@@ -5,8 +5,9 @@ class PPOSchedulerAgent:
     def __init__(self, model, horizon):
         self.H = horizon
         self.model = model
-        self.schedule_window = [0] * self.H
+        self.schedule_window = [-1] * self.H
         self.job_ids = {}
+        self.t = 0
 
     def plan(self, available_jobs, timestep):
 
@@ -19,35 +20,46 @@ class PPOSchedulerAgent:
         return schedule
 
     def reset(self):
-        self.schedule_window = [0] * self.H
+        self.schedule_window = [-1] * self.H
         self.job_ids = {}
+        self.t = 0
 
     def get_schedule_window(self):
         return self.schedule_window
 
     def embed_schedule(self):
-        schedule_embedding = [[0, 0, 0] if x == 0 else [self.job_ids[x][0].task_category, 
-                                                        self.job_ids[x][0].deadline_time, 
-                                                        self.job_ids[x][0].base_reward] for x in self.schedule_window]
+        schedule_embedding = [[-1, 0] if x == -1 else [self.job_ids[x][0].task_id, 
+                                                    self.job_ids[x][0].deadline_time - self.t] for x in self.schedule_window]
         return schedule_embedding
     
     def insert_task(self, job):
         # change to encode based on types
         schedule_tensor = torch.tensor(self.embed_schedule())
-        job_tensor = torch.tensor(self.encode_job(job))
+        job_tensor = torch.tensor([job.task_id, job.deadline_time - self.t] if job else [-1, 0])
         pred_length = self.model.get_pred_length(job_tensor)
         mask_tensor = torch.tensor(self.valid_mask(pred_length))
 
-        action, _, _ = self.model.get_action(schedule_tensor, job_tensor, mask_tensor)
-
+        action, _ = self.model.get_action(schedule_tensor, job_tensor, mask_tensor)
         action_item = action.item()
 
-        self.place(job.instance_id, pred_length, action_item)
+        self.place(job, pred_length, action_item)
 
-        self.job_ids[job.instance_id] = [job, pred_length]
+    def check_reward(self, job_id):
+        job, pred_length, progress = self.job_ids[job_id]
+        if progress == job.duration_time:
+            return job.get_reward(self.t)
+        elif progress > pred_length and progress < job.duration_time:
+            return -1
+        return 0
+    
+    def free_future_instances(self, job_id):
+        for i in range(self.H):
+            if self.schedule_window[i] == job_id:
+                self.schedule_window[i] = -1
 
-    def encode_job(self, job):
-        job_vals = [job.task_category, job.deadline_time, job.base_reward] if job else [0, 0, 0]
+    def encode_job(self, job_id):
+        job = self.job_ids[job_id][0]
+        job_vals = [job.task_id, job.deadline_time - self.t] if job else [-1, 0]
         return job_vals
 
     def valid_mask(self, pred_length):
@@ -56,26 +68,32 @@ class PPOSchedulerAgent:
             if i + pred_length > self.H:
                 mask.append(0)
             else:
-                ok = all(self.schedule_window[i + k] == 0 for k in range(pred_length))
+                ok = all(self.schedule_window[i + k] == -1 for k in range(pred_length))
                 mask.append(1 if ok else 0)
         return mask
 
     def can_place(self, i, pred_length):
         if i + pred_length > self.H:
             return False
-        return all(self.schedule_window[i + k] == 0 for k in range(pred_length))
+        return all(self.schedule_window[i + k] == -1 for k in range(pred_length))
 
-    def place(self, job_id, pred_length, i):
+    def place(self, job, pred_length, i):
+        if not job.instance_id in self.job_ids:
+            self.job_ids[job.instance_id] = [job, pred_length, 0]
         for k in range(pred_length):
-            self.schedule_window[i + k] = job_id
+            self.schedule_window[i + k] = job.instance_id
 
     def shift(self):
         job = self.schedule_window[0]
-        self.schedule_window = self.schedule_window[1:] + [0]
+        if job != -1:
+            self.job_ids[job][2] += 1
+        self.schedule_window = self.schedule_window[1:] + [-1]
+        self.t += 1
         return job
     
     def copy(self):
         s = PPOSchedulerAgent(self.model, self.H)
         s.schedule_window = self.schedule_window.copy()
         s.job_ids = self.job_ids.copy()
+        s.t = self.t
         return s
